ce1ccf7d2d82865f4dcb71d8a9eca47ba90c858d
lostNIII3 <73011138+NitinSirsath@users.noreply.github.com>
2022-03-29 14:33:50 +0530

    added social links
    added linkedin and instagram links

--- src/components/aboutus/SocialIcon.jsx
+++ src/components/aboutus/SocialIcon.jsx
@@ -32,7 +32,7 @@ const SocialIcon = () => {
   return (
     <Container>
         {data.map((ele)=>{
-            return <CardContainer key={ele.id} color={ele.bg}><Card  background={ele.img} ></Card></CardContainer>
+            return <CardContainer key={ele.id} color={ele.bg}><a href={ele.link}><Card  background={ele.img} ></Card></a></CardContainer>
         })}
     </Container>
   )
--- src/components/aboutus/socialicondata.js
+++ src/components/aboutus/socialicondata.js
@@ -4,22 +4,26 @@ const data = [
     {
         id: 1,
         img: './assets/icons/instagram.png',
-        bg:' #FF19A3;'
+        bg:' #FF19A3;',
+        link : 'https://www.instagram.com/upsilon_india/'
     },
     {
         id: 2,
         img: './assets/icons/facebook.png',
-        bg:'#2F7BEA;'
+        bg:'#2F7BEA;',
+        link: ''
     },
     {
         id: 3,
         img: './assets/icons/youtube.png',
-        bg:'#FF002E;'
+        bg:'#FF002E;',
+        link: ''
     },
     {
         id: 4,
         img: './assets/icons/linkedin.png',
-        bg:'#2F7BEA;'
+        bg:'#2F7BEA;',
+        link: 'https://www.linkedin.com/company/upsilon-india/about/'
     }
 ]
 export default data;
